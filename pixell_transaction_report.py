4b6b07bf19526fb5b6d9ff1625b6f597e30b04c1
Zander Santos <zsantos@RRC.ca>
2023-10-16 13:21:12 -0500

    Implemented Exception Handling for the pixell transaction report file

--- pixell_transaction_report.py
+++ pixell_transaction_report.py
@@ -20,61 +20,66 @@ error_message = ''
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
+try:
+    with open('bank_data.csv', 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        for row in reader:
+            # Reset valid record and error message for each iteration
+            valid_record = True
+            error_message = ''
 
-with open('bank_data.csv', 'r') as csv_file:
-    reader = csv.reader(csv_file)
-    for row in reader:
-        # Reset valid record and error message for each iteration
-        valid_record = True
-        error_message = ''
-
-        # Extract the customer ID from the first column
-        customer_id = row[0]
-        
-        # Extract the transaction type from the second column
-        transaction_type = row[1]
-        ### VALIDATION 1 ###
-
-        # Extract the transaction amount from the third column
-        ### VALIDATION 2 ###
-        transaction_amount = float(row[2])
-
-        if valid_record:
-            # Initialize the customer's account balance if it doesn't already exist
-            if customer_id not in customer_data:
-                customer_data[customer_id] = {'balance': 0, 'transactions': []}
-
-            # Update the customer's account balance based on the transaction type
-            elif transaction_type == 'deposit':
-                customer_data[customer_id]['balance'] += transaction_amount
-                transaction_count += 1
-                total_transaction_amount += transaction_amount
-            elif transaction_type == 'withdrawal':
-                customer_data[customer_id]['balance'] += transaction_amount
-                transaction_count += 1
-                total_transaction_amount += transaction_amount
+            # Extract the customer ID from the first column
+            customer_id = row[0]
             
-            # Record  transactions in the customer's transaction history
-            customer_data[customer_id]['transactions'].append((transaction_amount, transaction_type))
+            # Extract the transaction type from the second column
+            transaction_type = row[1]
+            ### VALIDATION 1 ###
+
+            # Extract the transaction amount from the third column
+            ### VALIDATION 2 ###
+            transaction_amount = float(row[2])
+
+            if valid_record:
+                # Initialize the customer's account balance if it doesn't already exist
+                if customer_id not in customer_data:
+                    customer_data[customer_id] = {'balance': 0, 'transactions': []}
+
+                # Update the customer's account balance based on the transaction type
+                elif transaction_type == 'deposit':
+                    customer_data[customer_id]['balance'] += transaction_amount
+                    transaction_count += 1
+                    total_transaction_amount += transaction_amount
+                elif transaction_type == 'withdrawal':
+                    customer_data[customer_id]['balance'] += transaction_amount
+                    transaction_count += 1
+                    total_transaction_amount += transaction_amount
+                
+                # Record  transactions in the customer's transaction history
+                customer_data[customer_id]['transactions'].append((transaction_amount, transaction_type))
+            
+            ### COLLECT INVALID RECORDS ###
+            
+
+
+    print("PiXELL River Transaction Report\n===============================\n")
+    # Print the final account balances for each customer
+    for customer_id, data in customer_data.items():
+        balance = data['balance']
+
+        print(f"\nCustomer {customer_id} has a balance of {balance}.")
+        # Print the transaction history for the customer
+        print("Transaction History:")
+        for transaction in data['transactions']:
+            amount, type = transaction
+            print(f"\t{type.capitalize()}: {amount}")
+
+    print(f"\nAVERAGE TRANSACTION AMOUNT: {(total_transaction_amount / transaction_counter)}")
+
+    print("\nREJECTED RECORDS\n================")
+    for record in rejected_records:
+        print("REJECTED:", record)
         
-        ### COLLECT INVALID RECORDS ###
-        
-
-
-print("PiXELL River Transaction Report\n===============================\n")
-# Print the final account balances for each customer
-for customer_id, data in customer_data.items():
-    balance = data['balance']
-
-    print(f"\nCustomer {customer_id} has a balance of {balance}.")
-    # Print the transaction history for the customer
-    print("Transaction History:")
-    for transaction in data['transactions']:
-        amount, type = transaction
-        print(f"\t{type.capitalize()}: {amount}")
-
-print(f"\nAVERAGE TRANSACTION AMOUNT: {(total_transaction_amount / transaction_counter)}")
-
-print("\nREJECTED RECORDS\n================")
-for record in rejected_records:
-    print("REJECTED:", record)
+except FileNotFoundError:
+    print("ERROR: {FileNotFoundError}")
+except Exception as e:
+    print("ERROR:",e)
